b444a93ab6c16610a175c51cb07e898e12451004
Wahelelm <welmazzouji72@gmail.com>
2025-03-29 17:24:22 +0100

    modif application

--- TakuzuGame/app.R
+++ TakuzuGame/app.R
@@ -1,57 +1,13 @@
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyalert)
 
-source("R/code.R")
 
-ui <- fluidPage(
-  titlePanel("Takuzu Game"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("niveau", "Choisissez un niveau :", 
-                  choices = c("Débutant", "Amateur", "Intermédiaire", "Expert")),
-      
-      actionButton("generer", "Générer la grille"),
-      
-      actionButton("verifier", "Vérifier la grille")
-    ),
-    mainPanel(
-      tableOutput("grillePuzzle"),
-      br(),
-      textOutput("resultat")
-    )
-  )
-  
-)
+# Charger les fichiers interface et serveur
+source("R/interface.R")
+source("R/serveur.R")
 
-server <- function(input, output, session){
-  # on la stocke dans un reactiveVal pour qu'elle puisse changer dynamiquement
-  puzzle <- reactiveVal()
-  
-  observeEvent(input$generer, {
-    nouveau_puzzle <- creer_puzzle(input$niveau)
-    puzzle(nouveau_puzzle)
-  })
-  
-  output$grillePuzzle <- renderTable({
-    p <- puzzle()
-    if (is.null(p)) {
-      return(NULL)
-    }
-    p
-  }, rownames = TRUE)
-  
-  observeEvent(input$verifier, {
-    p <- puzzle()
-    if (is.null(p)) {
-      output$resultat <- renderText("Aucune grille chargée. Cliquez sur 'Générer'.")
-      return()
-    }
-    est_valide <- verifier(p)
-    if (est_valide) {
-      output$resultat <- renderText("Bravo, la grille est valide !")
-    } else {
-      output$resultat <- renderText("Cette grille n'est pas valide (ou pas complète).")
-    }
-  })
-}
+# Lancer l'application Shiny
+shinyApp(ui = interface, server = server)
 
-shinyApp(ui, server)
